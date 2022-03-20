d07cef501583e69afd43d6b43e48ac210c9899f3
gulfaraj09 <94758299+gulfaraj09@users.noreply.github.com>
2022-03-21 03:40:42 +0530

    Create emoloyeeManager.c

---
+++ emoloyeeManager.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int chars, i = 0;
+    char *ptr;
+    char a, b;
+    while (i < 3)
+    {
+        printf("Employee %d : Enter the number of charcters in your employee id :\n", i);
+        scanf("%d", &chars);
+        getchar();
+        printf("Enter the value of a \n");
+        scanf("%c", &a);
+        getchar();
+        printf("Enter the value of b \n");
+        scanf("%c", &b);
+        ptr = (char *)malloc((chars + 1) * sizeof(char));
+        printf("Enter your Employe Id\n");
+        scanf("%s", ptr);
+        printf("Your employee id is %s\n", ptr);
+        free(ptr);
+        i = i + 1;
+    }
+
+    return 0;
+}
